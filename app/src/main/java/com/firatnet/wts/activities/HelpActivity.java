8e43b409fcc3002898cf27c2209f769fcd51cb20
Ghiath Mutlak <Ghiath.mutlak@gmail.com>
2019-06-10 18:03:43 +0300

    Send help messages

--- app/src/main/java/com/firatnet/wts/activities/HelpActivity.java
+++ app/src/main/java/com/firatnet/wts/activities/HelpActivity.java
@@ -3,24 +3,55 @@ package com.firatnet.wts.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.firatnet.wts.R;
+import com.firatnet.wts.database.SafetyDbHelper;
+import com.firatnet.wts.entities.Phone;
+
+import java.util.ArrayList;
 
 public class HelpActivity extends AppCompatActivity {
 
-    private Button help_btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
-        help_btn=findViewById(R.id.help_btn);
+
+        SafetyDbHelper helper = new SafetyDbHelper(getApplicationContext());
+        final ArrayList<Phone> phones = helper.getAllPhones();
+
+        Button help_btn = findViewById(R.id.help_btn);
         help_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                for (Phone phone: phones) {
+                    sendSMS(phone.getNumber(), "Help me please");
+                }
             }
         });
+
     }
+
+
+
+    public void sendSMS(String phoneNo, String msg) {
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, msg, null, null);
+            Toast.makeText(getApplicationContext(), "Message Sent",
+                    Toast.LENGTH_LONG).show();
+        } catch (Exception ex) {
+            Toast.makeText(getApplicationContext(),ex.getMessage(),
+                    Toast.LENGTH_LONG).show();
+            ex.printStackTrace();
+        }
+    }
+
+
 }
